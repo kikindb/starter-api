cd774ca680a2be6e9e8bc67a34ca499002665726
Enrique Dick <enriquedb2@gmail.com>
2020-08-20 23:52:24 -0500

    Password encription ready

---
+++ hash.js
@@ -0,0 +1,6 @@
+const bcrypt = require('bcrypt');
+
+async function run() {
+  const salt = await bcrypt.genSalt(10);
+  const hashed = await bcrypt.hash('password', salt);
+}
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
 const { User, validate } = require('../models/user');
 const mongoose = require('mongoose');
 const express = require('express');
@@ -17,16 +19,12 @@ router.post('/', async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send('User already registered.');
 
-  user = new User({
-    name: req.body.name,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: req.body.password
-  });
-
+  user = new User(_.pick(req.body, ['name', 'lastName', 'email', 'password']));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
-  res.send(user);
+  res.send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 router.put('/', async (req, res) => {
